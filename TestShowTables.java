489c9d9adad0b365a81b3a0e1dadb72a98a51dad
25021858-svg <25021858@vnu.edu.vn>
2026-05-28 02:58:51 +0700

    Add files via upload

---
+++ TestShowTables.java
@@ -0,0 +1,28 @@
+import dao.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestShowTables {
+    public static void main(String[] args) {
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT name FROM sqlite_master WHERE type='table'"
+            );
+
+            System.out.println("Danh sach bang trong database:");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ UserNotFoundException.java
@@ -0,0 +1,5 @@
+package exception;
+class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String messsage){super(messsage);}
+
+}
